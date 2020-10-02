5b96e1a55039c49efe8b8fc84a3b572029454994
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-10-02 15:21:07 +0200

    Created collision detection within the fall function to detect collision between doodler and platform

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
     let platforms = []
     let upTimerId
     let downTimerId
+    let isJumping = true
 
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
+        doodlerLeftSpace = platforms[0].left
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
@@ -52,6 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function jump() {
         clearInterval(downTimerId)
+        isJumping = true
         upTimerId = setInterval(function () {
             doodlerBottomSpace += 20
             doodler.style.bottom = doodlerBottomSpace + 'px'
@@ -63,12 +66,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function fall() {
         clearInterval(upTimerId)
+        isJumping = false
         downTimerId = setInterval(function () {
             doodlerBottomSpace -= 5
             doodler.style.bottom = doodlerBottomSpace + 'px'
             if (doodlerBottomSpace <= 0) {
                 gameOver()
             }
+            //collision detection
+            platforms.forEach(platform => {
+                if (
+                    (doodlerBottomSpace >= platform.bottom) &&
+                    (doodlerBottomSpace <= platform.bottom + 15) &&
+                    ((doodlerLeftSpace + 60)>= platform.left) &&
+                    (doodlerLeftSpace <= (platform.left + 85)) &&
+                    !isJumping
+                ) {
+                    console.log('landed')
+                    jump()
+                }
+            })
 
         }, 30)
     }
@@ -79,10 +96,21 @@ document.addEventListener('DOMContentLoaded', () => {
         clearInterval(downTimerId)
     }
 
+    function control(e) {
+        if (e.key === "ArrowLeft") {
+            //move left
+        } else if (e.key === "ArrowRight")
+        {
+            //move right
+        } else if (e.key === "ArrowUp") {
+            //moveStraight
+        }
+    }
+
     function start() {
         if (!isGameOver) {
-            createDoodler()
             createPlatforms()
+            createDoodler()
             setInterval(movePlatforms, 30)
             jump()
         }
