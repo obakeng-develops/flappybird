b850442cd8dbd1c584142854f286bdcbdd6e6e87
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-10-02 15:06:42 +0200

    Created movePlatform function to dynamically move the platform every 30 seconds

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
-    let doodlerBottomSpace = 150
+    let doodlerBottomSpace = 250
     let isGameOver = false
     let platformCount = 5
+    let platforms = []
+    let upTimerId
+    let downTimerId
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -16,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     class Platform {
         constructor(newPlatBottom) {
             this.bottom = newPlatBottom
-            this.left = math.random() * 315
+            this.left = Math.random() * 315
             this.visual = document.createElement('div')
 
             const visual = this.visual
@@ -30,16 +33,35 @@ document.addEventListener('DOMContentLoaded', () => {
     function createPlatforms() {
         for(let i = 0; i < platformCount; i++) {
             let platGap = 600 / platformCount
-            let newPlatBottom = 100 + i * platformGap
-            let newPlatform = new Platform()
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform(newPlatBottom)
+            platforms.push(newPlatform)
 
         }
+    }
+
+    function movePlatforms() {
+        if (doodlerBottomSpace > 200) {
+            platforms.forEach(platform => {
+                platform.bottom -= 4
+                let visual = platform.visual
+                visual.style.bottom = platform.bottom + 'px'
+            })
+        }
+    }
+
+    function jump() {
+        upTimerId = setInterval(function () {
+
+        }, 30)
     }
 
     function start() {
         if (!isGameOver) {
             createDoodler()
             createPlatforms()
+            setInterval(movePlatforms, 30)
+            jump()
         }
     }
     //attach to button
