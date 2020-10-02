bcf6495151c4085288e7c2a5ff8eecfe0bea7f1b
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-10-02 15:11:43 +0200

    Created fall and jump functions for the doodler

--- app.js
+++ app.js
@@ -51,9 +51,32 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function jump() {
+        clearInterval(downTimerId)
         upTimerId = setInterval(function () {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace > 350) {
+                fall()
+            }
+        }, 30)
+    }
+
+    function fall() {
+        clearInterval(upTimerId)
+        downTimerId = setInterval(function () {
+            doodlerBottomSpace -= 5
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace <= 0) {
+                gameOver()
+            }
 
         }, 30)
+    }
+
+    function gameOver() {
+        isGameOver = true
+        clearInterval(upTimerId)
+        clearInterval(downTimerId)
     }
 
     function start() {
